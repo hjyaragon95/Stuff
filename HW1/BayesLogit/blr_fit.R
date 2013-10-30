47b53a239e747ee13937513a4fbee1a48f5bce10
hjyaragon95 <hjyhuang@ucdavis.edu>
2013-10-30 14:06:54 -0700

    problem 2

--- HW1/BayesLogit/blr_fit.R
+++ HW1/BayesLogit/blr_fit.R
@@ -1,10 +1,9 @@
-#library("gdata")
+library("lattice")
+library("coda")
 library("MASS")
 library("mvtnorm")
-#library("MCMCpack")
-#library("debug")
-#data1<-read.csv("/Users/aragon95/Stuff/hw1/bayeslogit/data/blr_data_1001.csv")
-#data2<-read.csv("/Users/aragon95/Stuff/hw1/bayeslogit/data/blr_pars_1001.csv")
+
+#set the data reading in Gauss
 args <- commandArgs(TRUE)
 
 cat(paste0("Command-line arguments:\n"))
@@ -27,79 +26,106 @@ if (length(args)==0){
 }
 filename=paste0("data/blr_data_",sim_num,".csv")
 data1<-read.csv(filename)
-#version 2   with 
 
 
+
+# the bayes.logreg function, output  a list{beta_1, beta_2, and v_sq}
 bayes.logreg<-function(m,y,X,beta.0, Sigma.0.inv,
                        niter=10000,burnin=1000,print.every=1000,
-                       retune=100,verbose=TRUE,sim_num) {
+                       retune=100,verbose=TRUE) {
   #intial a dataset to store beta 
   draws<-data.frame()
+  #set the datatype to better fit my code
   X=as.matrix(X)
   beta.0=as.matrix(beta.0,nrow=2)
+  
   #logit function
   logit<-function(u){
     ratio=exp(u)/(1+exp(u))
   }
-  #browser()
-  #beta.update function
+  
+  #beta.update function: 
   beta.update<-function(beta.cur,v_sq){
-    #browser()
+    # sample beta.star from the normal distribution with mean=beta.cur and Sigma=v_sq
     beta.star<-mvrnorm(n=1,mu=beta.cur, Sigma=v_sq)
-    pdf.y.beta.cur=sum(dbinom(y,size=m,prob=logit(X%*%beta.cur)/(1+logit(X%*%beta.cur)),log=TRUE))
-    pdf.y.beta.star=sum(dbinom(y,size=m,prob=logit(X%*%beta.star)/(1+logit(X%*%beta.star)),log=TRUE))
+    #calculate the accept.prob
+    pdf.y.beta.cur=sum(dbinom(y,size=m,prob=logit(X%*%beta.cur),log=TRUE))
+    pdf.y.beta.star=sum(dbinom(y,size=m,prob=logit(X%*%beta.star),log=TRUE))
     pdf.beta.star=dmvnorm(as.vector(beta.star),mean=c(0,0),sigma=diag(1,2),log=TRUE)
     pdf.beta.cur=dmvnorm(as.vector(beta.cur),mean=c(0,0),sigma=diag(1,2),log=TRUE)
     accept.prob=min(0,pdf.y.beta.star+pdf.beta.star-(pdf.y.beta.cur+pdf.beta.cur))
+    #decide whether to accept beta.star or not
     if(log(runif(1))< accept.prob)
       return(as.matrix(beta.star,nrow=2))
     else 
       return(beta.cur)
   }
+  
+  
   # burnin period and tune
+  #set up the initial value for the burnin period
   beta.cur=beta.0
   v_sq=diag(1,2)
   v_sq_ls<-list()
   accept=0
+  #for loop for the burn in and tuning
   for(i in 1: burnin){
     betaup1<-beta.update(beta.cur,v_sq)
+    #count the acceptence time 
     if(!all(betaup1==beta.cur))
       accept=accept+1
     else
       accpet=accept
+    #record the updated beta
     draws<- rbind(draws, t(beta.cur<-betaup1)) 
     # update v_sq
     if(i%% retune==0){
       v_sq_ls[[i %/% retune]]=v_sq
-     if(accept/retune<0.3 )
+      if(accept/retune<0.3 )
         v_sq=v_sq/exp(1)
       else if(accept/retune>0.6)
         v_sq=v_sq*exp(1)
       accept=0
-  }
+    }
   }
   
   
-  #browser()
-  ##############iteration 
+  ##############iteration period
+  accept=0
   for(j in 1:niter){
-    draws<- rbind(draws, t(beta.cur<-beta.update(beta.cur,v_sq)))
+    betaup1<-beta.update(beta.cur,v_sq)
+    #count the accept time
+    if(!all(betaup1==beta.cur))
+      accept=accept+1
+    else
+      accpet=accept
+    #store the result in the draws
+    draws<- rbind(draws, t(beta.cur<-betaup1))
+    #print every 1000 iteration is finished
     if(j%%1000==0){cat(j,'finished','\n')}
+
   }  
   
-  beta1_q=quantile(draws[-c(1:1000),1],prob=seq(0.01,0.99,0.01))
-  beta2_q=quantile(draws[-c(1:1000),2],prob=seq(0.01,0.99,0.01))
-  return(cbind(beta1_q,beta2_q))
-  write.csv(as.data.frame(cbind(beta1_q,beta2_q)), file=paste0("results/blr_res_",sim_num,".csv"))
+  return(list(draws[,1],draws[,2],v_sq_ls,accpet/niter))
   
 }
 
 
+## run the bayes.logreg function using 
 result<-bayes.logreg(m=data1$n,y=data1$y,
                      X=as.matrix(data1[,c(3,4)]),
                      beta.0=as.matrix(c(0,0),nrow=2),
                      Sigma.0.inv=solve(diag(1,2)))
+
+#Trellis plot for mcmc result
+plot(mcmc(result[[1]],start=1001))
+plot(mcmc(result[[2]],start=1001))
+result[[3]]
+result[[4]]
+beta1_q=quantile(result[[1]],probs=seq(0.01,0.99,0.01))
+beta2_q=quantile(result[[2]],probs=seq(0.01,0.99,0.01))
+
 #write.csv(result, file=paste0("results/blr_res_",sim_num,".csv"),col.names=FALSE,row.names=FALSE)
-write.table(result, file=paste0("results/blr_res_",sim_num,".csv"),quote=FALSE, row.names=FALSE, col.names=FALSE, sep=",")
+write.table(cbind(beta1_q,beta2_q), file=paste0("results/blr_res_",sim_num,".csv"),quote=FALSE, row.names=FALSE, col.names=FALSE, sep=",")
 
 #write.table(data.frame(data here),file=filename,sep=",",quote=FALSE,row.names=FALSE,col.names=FALSE)
